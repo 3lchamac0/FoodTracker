21484665858fa37e890f8db22a16d3f8d53782af
vinodbhavya <vinodbhavya410@gmail.com>
2019-10-25 16:08:15 +0100

    clase 5

---
+++ FoodTracker/UIStackView.swift
@@ -0,0 +1,21 @@
+//
+//  UIStackView.swift
+//  FoodTracker
+//
+//  Created by formando on 25/10/2019.
+//  Copyright © 2019 formando. All rights reserved.
+//
+
+import UIKit
+
+class UIStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

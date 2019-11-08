d0a91db98a9953a7bc90a39c0dd47f792a6d3acf
vinodbhavya <vinodbhavya410@gmail.com>
2019-11-08 15:37:04 +0000

    lab 5

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by formando on 08/11/2019.
+//  Copyright © 2019 formando. All rights reserved.
+//
+
+import Foundation
---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by formando on 08/11/2019.
+//  Copyright © 2019 formando. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
